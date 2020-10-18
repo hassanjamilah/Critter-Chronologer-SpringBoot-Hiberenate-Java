20ce234c2820c5bcef59f1843a1470084ca43693
hassanjamilah <hassanjamilahj@gmail.com>
2020-10-18 10:10:53 +0800

    Created table schedule

--- critter/src/main/java/com/udacity/jdnd/course3/critter/entitiy/Schedule.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/entitiy/Schedule.java
@@ -1,4 +1,27 @@
 package com.udacity.jdnd.course3.critter.entitiy;
 
+import org.checkerframework.checker.index.qual.GTENegativeOne;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+@Entity
 public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToMany
+    private List<Employee> employees;
+
+    @ManyToMany
+    private List<Pet> pets;
+
+    private LocalDate scheduleDate;
+
+    @ManyToMany
+    private Set<Activity> activities;
+
 }
